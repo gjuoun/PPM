6d60abc0c01675fcac58eaf9d59a05a1964fd901
Jun Guo <gjuoun@gmail.com>
2019-03-11 07:59:37 -0300

    done the prototype

--- js/myjs.js
+++ js/myjs.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+$("#reset").on("click", function() {
+  $('label').removeClass('active');
+});
