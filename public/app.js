b9d065a9f34b3bf191a9b13cc07aacd05be6351f
Jun Guo <gjuoun@gmail.com>
2019-03-28 01:54:06 -0300

    integrated with Firebase removed the login page element now using google auth login

--- public/app.js
+++ public/app.js
@@ -1,9 +1,55 @@
 // short hand for querySelector
-const qs = (elementName) => document.querySelector(elementName)
+let message = []
+
+const $ = (elementName) => document.querySelector(elementName)
+const eventListen = (element, event, callback) => {
+  document.querySelector(element).addEventListener(event, callback)
+}
+
 
 document.addEventListener('DOMContentLoaded', (e) => {
   const app = firebase.app()
+  
+  const db = firebase.firestore()
+  
+  const myPostsRef = db.collection('posts')
+  
+  const query = myPostsRef.orderBy('createdAt', 'desc').limit(2)
+  
+  // myPost.get()
+  //   .then((doc) => {
+  //     const data = doc.data()
+  //     console.log(data)
+  //   })
+  
+  // listen to the stream and get update
+  // myPostsRef.onSnapshot((doc) => {
+  //   const data = doc
+  //   console.log(data)
+  //
+  //
+  // })
+  
+  // query.get().then((posts) => {
+  //   posts.forEach((doc) => {
+  //     console.log(doc.data())
+  //   })
+  // })
+  
+  query.onSnapshot((posts) => {
+    posts.forEach((doc) => {
+      console.log(doc.data())
+    })
+  })
 })
+
+
+function updateTitle(e) {
+  const db = firebase.firestore()
+  const myPost = db.collection('posts').doc('firstpost')
+  
+  myPost.update({title: e.target.value})
+}
 
 
 async function firebaseAuth() {
@@ -14,16 +60,27 @@ async function firebaseAuth() {
   
   console.log(user)
   
-  let userinfoEl = qs('#p-userinfo')
+  let userinfoEl = $('#p-userinfo')
   userinfoEl.textContent = `Hello, ${user.displayName}`
   
   getCurrentUser()
 }
 
 function getCurrentUser() {
-  let uid  = 'K7XiltbklZSw4rYk2zGpPyCCflr2'
+  let uid = 'K7XiltbklZSw4rYk2zGpPyCCflr2'
   let user = firebase.auth().currentUser
   
   console.log('current user', user)
 }
 
+
+function putNewPost() {
+  const db = firebase.firestore()
+  const postRef = db.collection('posts')
+  
+  let timeNow = new firebase.firestore.Timestamp(seconds = 1553698813)
+  
+  postRef.add({title: 'time item', createdAt: timeNow}).then((doc) => {
+    console.log(doc)
+  })
+}
---
+++ public/js/myjs.js
@@ -0,0 +1,73 @@
+// helper functions
+const $ = (elementName) => document.querySelector(elementName)
+const eventListen = (element, event, callback) => {
+  document.querySelector(element).addEventListener(event, callback)
+}
+const documentReady = (callback) => {
+  document.addEventListener('DOMContentLoaded', callback)
+}
+// global elements
+const loginPage = $('#login-container')
+const mainPage = $('#mainpage-container')
+const chatPage = $('#chat-container')
+//-----
+const loginButton = $('#button-login')
+
+
+// firebase shortcuts
+const getCurrentUser = () => firebase.auth().currentUser
+
+// global variables
+let db, app, user
+
+
+// shortcuts for page display
+const show = (el) => {
+  el.style.display = 'block'
+}
+const hide = (el) => {
+  el.style.display = 'none'
+}
+
+
+documentReady(() => {
+  // activate side nav
+  var elems = $('.sidenav')
+  var instances = M.Sidenav.init(elems, {edge: 'left', draggable: true})
+  
+  app = firebase.app()
+  db = firebase.firestore()
+  
+  show(loginPage)
+  hide(mainPage)
+  hide(chatPage)
+  
+})
+
+
+loginButton.addEventListener('click', async (e) => {
+  e.preventDefault()
+  
+  const provider = new firebase.auth.GoogleAuthProvider()
+  await firebase.auth().signInWithPopup(provider)
+  
+  user = getCurrentUser()
+  
+  show(mainPage)
+  hide(chatPage)
+  hide(loginPage)
+  
+  updateSiderBarInformation()
+  updateDB()
+})
+
+
+function updateSiderBarInformation() {
+  $('#sidebar-displayName').textContent = user.displayName
+  $('#sidebar-email').textContent = user.email
+  $('#siderbar-avatar').setAttribute('src', user.photoURL)
+}
+
+function updateDB() {
+  
+}
